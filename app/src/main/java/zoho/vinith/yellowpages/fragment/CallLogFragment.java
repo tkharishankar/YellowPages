c8f937d8eaae6206e085cbff632552baef235954
Hari <tkharishankar@gmail.com>
2018-08-08 15:30:02 +0400

    permission and list fixed

--- app/src/main/java/zoho/vinith/yellowpages/fragment/CallLogFragment.java
+++ app/src/main/java/zoho/vinith/yellowpages/fragment/CallLogFragment.java
@@ -1,6 +1,7 @@
 package zoho.vinith.yellowpages.fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -23,8 +24,8 @@ import android.widget.Toast;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import zoho.vinith.yellowpages.R;
 import zoho.vinith.yellowpages.RecyclerItemClickListener;
@@ -35,8 +36,6 @@ import zoho.vinith.yellowpages.model.ContactInfo;
 
 public class CallLogFragment extends Fragment {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 10;
-    private static final int MY_PERMISSIONS_REQUEST_READ_CALL_LOG = 20;
     private String TAG = "Call Fragment";
 
     YellowPageDatabase dbHandler;
@@ -44,33 +43,60 @@ public class CallLogFragment extends Fragment {
     private RecyclerView recyclerView;
     private CallLogAdapter callLogAdapter;
 
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
+    private String[] permissions = {Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALL_LOG};
+
     @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean arePermissionsEnabled() {
+        for (String permission : permissions) {
+            if (getActivity().checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+        }
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestMultiplePermissions() {
+        List<String> remainingPermissions = new ArrayList<>();
+        for (String permission : permissions) {
+            if (getActivity().checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                remainingPermissions.add(permission);
+            }
+        }
+        requestPermissions(remainingPermissions.toArray(new String[remainingPermissions.size()]), PERMISSIONS_REQUEST_READ_CONTACTS);
+    }
+
+    public void populateContacts() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (arePermissionsEnabled()) {
+                injectCallLogList(callLogAdapter);
+            } else {
+                requestMultiplePermissions();
+            }
+        }
+    }
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG,"OnCreate()");
-
-        getActivity().checkSelfPermission(Manifest.permission.READ_CONTACTS);
-        if (getActivity().checkSelfPermission(Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            return;
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    if (shouldShowRequestPermissionRationale(permissions[i])) {
+                        Toast.makeText(getContext(), "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+                    }
+                    return;
+                }
+            }
+            populateContacts();
         }
-        getActivity().checkSelfPermission(Manifest.permission.READ_CALL_LOG);
-        if (getActivity().checkSelfPermission(Manifest.permission.READ_CALL_LOG)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CALL_LOG},
-                    MY_PERMISSIONS_REQUEST_READ_CALL_LOG);
-            return;
-        }
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d(TAG,"OnCreateView()");
+        Log.d(TAG, "OnCreateView()");
         return inflater.inflate(R.layout.layout_call_logs, container, false);
 
     }
@@ -79,7 +105,7 @@ public class CallLogFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Log.d(TAG,"OnViewCreated");
+        Log.d(TAG, "OnViewCreated");
 
         recyclerView = view.findViewById(R.id.call_recycler_view);
         dbHandler = new YellowPageDatabase(getActivity(), null, null, 1);
@@ -93,36 +119,42 @@ public class CallLogFragment extends Fragment {
         callLogAdapter.notifyDataSetChanged();
 
         recyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(getContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override public void onItemClick(View view, int position) {
+                new RecyclerItemClickListener(getContext(), recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
                         CallLogInfo callLogInfo = callLogInfoList.get(position);
                         dialPhoneNumber(callLogInfo.getPhone_Number());
                     }
 
-                    @Override public void onLongItemClick(View view, int position) {
+                    @Override
+                    public void onLongItemClick(View view, int position) {
                         // do whatever
                     }
                 })
         );
-
-        injectCallLogList(callLogAdapter);
+        populateContacts();
 
     }
+
 
     private void injectCallLogList(CallLogAdapter callLogAdapter) {
         ArrayList<ContactInfo> contactInfoList = new ArrayList<>();
         contactInfoList = dbHandler.getContactListfromDB();
-        Cursor managedCursor = getActivity().managedQuery(CallLog.Calls.CONTENT_URI, null,
-                null, null, null);
+        @SuppressLint("MissingPermission")
+        Cursor managedCursor = getActivity().getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                null, null, null, CallLog.Calls.DATE + " DESC");
         int number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
         int type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
         int date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
         int duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
-
         while (managedCursor.moveToNext()) {
             String phNumber = managedCursor.getString(number);
-            for(ContactInfo contactInfo : contactInfoList) {
-                if(contactInfo.getPhone_Number().equals(phNumber)) {
+            Log.i("number", "number:" + phNumber);
+            //+ "\ttype:" + callDayTime + "\tdate:" + dateOfCall + "\tduration:" + callDuration);
+
+            for (ContactInfo contactInfo : contactInfoList) {
+                Log.i("contactInfo" + contactInfo.getPhone_Number(), "contactInfo:" + phNumber);
+                if (checkMatch(contactInfo.getPhone_Number(), phNumber)) {
                     String callType = managedCursor.getString(type);
                     String callDate = managedCursor.getString(date);
                     Date callDayTime = new Date(Long.valueOf(callDate));
@@ -145,15 +177,22 @@ public class CallLogFragment extends Fragment {
                             dir = "MISSED";
                             break;
                     }
-                    callLogInfoList.add(new CallLogInfo(contactInfo.getName(),phNumber,contactInfo.getPhoto(),dateOfCall,dir,callDuration));
+                    callLogInfoList.add(new CallLogInfo(contactInfo.getName(), phNumber, contactInfo.getPhoto(), dateOfCall, dir, callDuration));
                 }
             }
         }
         //managedCursor.close();
-        Collections.reverse(callLogInfoList);
         callLogAdapter.setCallLogInfoList(callLogInfoList);
         callLogAdapter.notifyDataSetChanged();
 
+    }
+
+    private boolean checkMatch(String saved_number, String log_number) {
+        saved_number = saved_number.replaceAll("\\s+", "");
+        log_number = log_number.replaceAll("\\s+", "");
+        if (saved_number.equals(log_number))
+            return true;
+        else return false;
     }
 
     private static String timeConversion(int totalSeconds) {
@@ -166,58 +205,34 @@ public class CallLogFragment extends Fragment {
         int minutes = totalMinutes % MINUTES_IN_AN_HOUR;
         int hours = totalMinutes / MINUTES_IN_AN_HOUR;
 
-        return String.format("%02d:%02d:%02d", hours,minutes,seconds);
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG,"OnPause()");
+        Log.d(TAG, "OnPause()");
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG,"OnResume()");
+        Log.d(TAG, "OnResume()");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.d(TAG,"OnDestroyView()");
+        Log.d(TAG, "OnDestroyView()");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"OnDestroy()");
+        Log.d(TAG, "OnDestroy()");
     }
 
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! do the
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-            case MY_PERMISSIONS_REQUEST_READ_CALL_LOG:{
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                }
-                return;
-            }
-        }
-    }
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
